cce0875766f368f9af42452fc3f331bcb7ddcd24
AnsaYong <ansahmbomyong@gmail.com>
2023-12-19 02:34:29 +0200

    imported missing modules and methods

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, String
 from sqlalchemy import Column, Integer, String
 
 
