3422d9ad76bbed472063f1bbc4bddbfe4fcc97ce
AnsaYong <ansahmbomyong@gmail.com>
2023-12-19 12:11:38 +0200

    updated import statements

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, String
-from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
@@ -12,12 +12,13 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    
     id = Column(String(60), primary_key=True, nullable=False, unique=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -38,10 +39,9 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -56,4 +56,4 @@ class BaseModel:
 
     def delete(self):
         """Delete the current instance from the storage"""
-        storage.delete(self)
+        models.storage.delete(self)
