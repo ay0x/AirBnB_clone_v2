5a5bc42247cb902e41562dbce42378d6c2437c9f
Ayo Idoko <ayo.idoko007@gmail.com>
2024-01-14 02:57:41 +0200

    Updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,45 +1,22 @@
 #!/usr/bin/python3
-"""This module provides a function that generates
-a `.tgz` archive from the contents of the web_static
-folder of my AirBnB Clone repo.
-"""
-from fabric.api import local
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
-import os
+from fabric.api import local
 
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static
-    folder.
-
-    Returns:
-        str: Archive path if successfully generated, None otherwise.
-    """
-    # Create the versions folder if it doesn't exist
-    local("mkdir -p versions")
-
-    # Get the current date and time
-    now = datetime.utcnow()
-    formatted_date = now.strftime("%Y%m%d%H%M%S")
-
-    # Set the archive path
-    archive_path = "versions/web_static_{}.tgz".format(formatted_date)
-
-    # Compress the contents of the web_static folder into a .tgz archive
-    result = local("tar -czvf {} web_static".format(archive_path))
-
-    # Check if the compression was successful
-    if result.succeeded:
-        # Get the size of the archive
-        archive_size = os.path.getsize(archive_path)
-
-        # Print the archive path and size
-        print("web_static packed: {} -> {}Bytes".format(archive_path,
-                                                        archive_size))
-        return archive_path
-    else:
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
-
-# Example usage
-do_pack()
+    return file
