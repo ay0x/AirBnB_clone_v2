3e27811175f87237a56021ca1968235595c350e8
ay0x <85720179+ay0x@users.noreply.github.com>
2024-01-29 03:10:44 +0200

    Updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+- Runs Flask web application.
+- The application listens on 0.0.0.0, port 5000.
 """
 from models import storage
 from flask import Flask
@@ -14,9 +12,9 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
+    """
+    - Displays an HTML page with a list of all State objects in storage.
+    - States are sorted by name.
     """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
@@ -29,4 +27,5 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    """Starts Flask web application"""
+    app.run(host="0.0.0.0", port=5000)
